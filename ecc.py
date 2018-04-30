4b8e71fae0570f287abb400599a3651814c82070
adamjonas <jonas@flatironschool.com>
2018-04-30 17:19:42 -0400

    remove ecc tests

--- ecc.py
+++ ecc.py
@@ -151,7 +151,7 @@ class Point:
         # Result is point at infinity
         # Remember to return an instance of this class:
         # self.__class__(x, y, a, b)
- 
+
         # Case 2: self.x != other.x
         # Formula (x3,y3)==(x1,y1)+(x2,y2)
         # s=(y2-y1)/(x2-x1)
@@ -168,31 +168,3 @@ class Point:
         # Remember to return an instance of this class:
         # self.__class__(x, y, a, b)
         raise NotImplementedError
-
-
-class PointTest(TestCase):
-
-    def test_on_curve(self):
-        with self.assertRaises(RuntimeError):
-            Point(x=-2, y=4, a=5, b=7)
-        # these should not raise an error
-        Point(x=3, y=-7, a=5, b=7)
-        Point(x=18, y=77, a=5, b=7)
-
-    def test_add0(self):
-        a = Point(x=None, y=None, a=5, b=7)
-        b = Point(x=2, y=5, a=5, b=7)
-        c = Point(x=2, y=-5, a=5, b=7)
-        self.assertEqual(a+b, b)
-        self.assertEqual(b+a, b)
-        self.assertEqual(b+c, a)
-    
-    def test_add1(self):
-        a = Point(x=3, y=7, a=5, b=7)
-        b = Point(x=-1, y=-1, a=5, b=7)
-        self.assertEqual(a+b, Point(x=2, y=-5, a=5, b=7))
-
-    def test_add2(self):
-        a = Point(x=-1, y=1, a=5, b=7)
-        self.assertEqual(a+a, Point(x=18, y=-77, a=5, b=7))
-
