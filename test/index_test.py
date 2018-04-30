fd0132f097c4e1ce5501f9410747bdc1792908e4
adamjonas <jonas@flatironschool.com>
2018-04-30 17:16:39 -0400

    add test directory and break out ecc test

---
+++ test/index_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+class PointTest(TestCase):
+
+    def test_on_curve(self):
+        with self.assertRaises(RuntimeError):
+            Point(x=-2, y=4, a=5, b=7)
+        # these should not raise an error
+        Point(x=3, y=-7, a=5, b=7)
+        Point(x=18, y=77, a=5, b=7)
+
+    def test_add0(self):
+        a = Point(x=None, y=None, a=5, b=7)
+        b = Point(x=2, y=5, a=5, b=7)
+        c = Point(x=2, y=-5, a=5, b=7)
+        self.assertEqual(a+b, b)
+        self.assertEqual(b+a, b)
+        self.assertEqual(b+c, a)
+
+    def test_add1(self):
+        a = Point(x=3, y=7, a=5, b=7)
+        b = Point(x=-1, y=-1, a=5, b=7)
+        self.assertEqual(a+b, Point(x=2, y=-5, a=5, b=7))
+
+    def test_add2(self):
+        a = Point(x=-1, y=1, a=5, b=7)
+        self.assertEqual(a+a, Point(x=18, y=-77, a=5, b=7))
